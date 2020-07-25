196fce9291e5d8ecfa5e8baf64017372de87cf14
Fake hacker <67508057+fakehacker0504@users.noreply.github.com>
2020-07-25 16:19:44 +0530

    guessgame.py

---
+++ guessgame.py
@@ -0,0 +1,12 @@
+print("☆☆☆☆☆CREATED BY FAKE HACKER☆☆☆☆☆")
+
+# NUMBER GUESSER
+
+winning_number = 7
+
+user_input = input("guess a number b/w 1 and 10:")
+user_input =int(user_input)
+if user input == Winning_number
+    print("♤♤♤YOU WON♤♤♤")
+else:
+    print("BETTER LUCK NEXT TIME!!!")
